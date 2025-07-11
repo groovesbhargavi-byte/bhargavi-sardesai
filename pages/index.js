47103f0cbbb8aea15e8d97675a18599230515ff2
TharunRajeev <53303380+TharunRajeev@users.noreply.github.com>
2025-07-11 11:34:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import About from '../components/About';
 import Projects from '../components/Projects';
 import Connect from '../components/Connect';
 import Footer from '../components/Footer.jsx';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const { basePath } = useRouter();
   return (
     <div
       className="min-h-screen w-full bg-fixed bg-center bg-cover"
-      style={{ backgroundImage: `url('/hero.jpg')` }}
+      style={{ backgroundImage: `url('${basePath}/hero.jpg')` }}
     >
       <div className="bg-black/20 min-h-screen w-full">
         <Navbar />
