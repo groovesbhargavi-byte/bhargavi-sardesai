3fc8d9937982793753b00fc045a8e90b0e56ecd7
TharunRajeev <53303380+TharunRajeev@users.noreply.github.com>
2025-07-21 14:27:15 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,21 @@ import Footer from '../components/Footer.jsx';
 
 export default function Home() {
   return (
-    <div
-      className="
-        min-h-screen w-full
-        bg-no-repeat
-        bg-cover
-        bg-[position:70%_30%]
-        bg-fixed
-      "
-      style={{ backgroundImage: "url('/hero.jpg')" }}
-    >
-      <div className="bg-black/20">
+    <div className="relative w-full min-h-screen overflow-hidden">
+      {/* 🔒 Fixed background image (works on ALL devices) */}
+      <div className="fixed inset-0 z-0">
+        <img
+          src="/hero.jpg"
+          alt="Background"
+          className="w-full h-full object-cover object-[75%]"
+        />
+      </div>
+
+      {/* 🔳 Overlay to darken the image if needed */}
+      <div className="fixed inset-0 z-10 bg-black/30" />
+
+      {/* 🔼 Actual page content, scrolls above the fixed background */}
+      <div className="relative z-20">
         <Navbar />
         <Hero />
         <About />
@@ -29,4 +33,5 @@ export default function Home() {
     </div>
   );
 }
+
 
